6424ff3ac52f5780b3655d6a71d7a2edc7875bea
Barros263inf <barros263inf@gmail.com>
2025-06-01 03:50:03 -0300

    feat: Add support for environment variables

--- src/main/java/com/tempolivre/api/ApiApplication.java
+++ src/main/java/com/tempolivre/api/ApiApplication.java
@@ -1,14 +1,35 @@
 package com.tempolivre.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		// Carregar o .env
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
 
-		SpringApplication.run(ApiApplication.class, args);
+		// Mapear e definir as variaveis do sistema
+		Map<String, Object> props = new HashMap<>();
+		dotenv.entries().forEach(dotenvEntry -> {
+			String key = dotenvEntry.getKey();
+			String value = dotenvEntry.getValue();
+			System.setProperty(key, value);
+			props.put(key, value);
+		});
+
+		// Iniciar o Spring com as variaveis carregadas
+
+		SpringApplication app = new SpringApplication(ApiApplication.class);
+		app.setDefaultProperties(props);
+		app.run(args);
 	}
 
 }
